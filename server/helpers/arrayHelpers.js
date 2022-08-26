6ee4f8e2d6ff6910e5f98f4c05923ebed4578f9b
Segneal <znicolasheredia@gmail.com>
2022-08-26 10:47:50 -0300

    v2.0

--- server/helpers/arrayHelpers.js
+++
@@ -1,7 +0,0 @@
-const groupBy = (arr) => {
-  let property = "folder";
-  return arr.reduce((acc, cur) => {
-    acc[cur[property]] = [...(acc[cur[property]] || []), cur];
-    return acc;
-  }, []);
-};
--- src/services/galleries.js
+++ src/services/galleries.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { groupBy } from "../Assets/helpers/arrayHelpers";
 
+const API_URL = "https://jesi-server.herokuapp.com/getGalleries";
+
 export const getGalleries = async () => {
-  const res = await axios.get("http://localhost:5500/getGalleries");
+  const res = await axios.get(API_URL);
   return await groupBy(res.data.resources);
 };
