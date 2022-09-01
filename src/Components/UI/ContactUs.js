a93dc97988c4326227e875ce5dabc7840449be79
Segneal <znicolasheredia@gmail.com>
2022-09-01 00:25:48 -0300

    1.01h

--- src/Components/UI/ContactUs.js
+++ src/Components/UI/ContactUs.js
@@ -3,25 +3,30 @@ import emailjs from "@emailjs/browser";
 import "../../Assets/Styles/contact.css";
 import Instagram from "../../Assets/Images/logos/Instagram.png";
 import Mail from "../../Assets/Images/logos/Mail.png";
-import { Navigate } from "react-router-dom";
+import Spinner from "../../Assets/Images/Spinner.gif";
+import MessageModal from "./MessageModal";
 
 const SERVICE_ID = "service_ryj2mk3";
 const TEMPLATE_ID = "template_g3vsfqy";
 const PUBLIC_KEY = "bgsv6X6__kLcYffCS";
 
+const STATUS_RESPONSE = 200;
+
 export const ContactUs = () => {
+  const [loading, setLoading] = React.useState(false);
+  const [messageSent, setMessageSent] = React.useState(false);
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
-
+    setLoading(true);
     emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY).then(
       (result) => {
-        result.status === 200 ? (
-          <Navigate to="/" />
-        ) : (
-          console.log(result.status)
-        );
+        if (result.status === STATUS_RESPONSE) {
+          console.log("Email sent");
+          setMessageSent(true);
+          setLoading(false);
+        }
       },
       (error) => {
         console.log(error.text);
@@ -31,7 +36,7 @@ export const ContactUs = () => {
 
   return (
     <div className="contact-body">
-      <div className="contact-form">
+      <section className="contact-form">
         <div className="contact-title">
           <h1>Escribime</h1>
         </div>
@@ -53,28 +58,37 @@ export const ContactUs = () => {
             />
             <label>Mensaje</label>
             <textarea className="contact-input" name="message" required />
-            <button className="contact-button" type="submit" value="Send">
-              Enviar
-            </button>
+            {loading ? (
+              <div className="contact-sending-message">
+                <img src={Spinner} alt=""></img>
+              </div>
+            ) : (
+              <button className="contact-button" type="submit" value="Send">
+                Enviar
+              </button>
+            )}
+            {messageSent && <h1>Mensaje Enviado</h1>}
           </div>
         </form>
-      </div>
-      <div className="contact-info-wrapper">
+      </section>
+      <section className="contact-info-wrapper">
         <div className="contact-title">
           <h1>Informacion de contacto</h1>
         </div>
         <p>Cualquier sugerencia es bienvenida.</p>
-        <div className="contact-icons">
+        <span className="contact-container">
           <div className="icons">
             <img src={Instagram} alt=""></img>
-            <h1>@irene_moda_ybelleza</h1>
           </div>
+          <h1>@irene_moda_ybelleza</h1>
+        </span>
+        <span className="contact-container">
           <div className="icons">
             <img src={Mail} alt=""></img>
-            <h1>irenevalleji25@gmail.com</h1>
           </div>
-        </div>
-      </div>
+          <h1>irenevalleji25@gmail.com</h1>
+        </span>
+      </section>
     </div>
   );
 };
